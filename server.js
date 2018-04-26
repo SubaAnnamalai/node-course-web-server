d430c8672165f8aef4ba171f0176492f908f4705
SubaAnnamalai <suba.annamalai@peerform.com>
2018-04-26 10:28:57 -0400

    minor project page corrections

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/projects', (req, res) => {
-  res.render('projects.hbs', {
+  res.render('Projects.hbs', {
     pageTitle: 'Projects Page',
     welcomeMessage: 'Portfolio Page!'
   });
