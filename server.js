a6171b3c3973d3e0ae9099a46f0b807b18d33ca7
SubaAnnamalai <suba.annamalai@peerform.com>
2018-04-26 10:18:36 -0400

    project page changes

--- server.js
+++ server.js
@@ -43,6 +43,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    welcomeMessage: 'Portfolio Page!'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
       errorMessage : 'Unable to fulfil this request.'
